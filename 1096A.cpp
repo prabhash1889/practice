fbc8d0d94bce4696132645f1a743b173f6f04ccc
prabhashpadhan <prabhashpadhan1889@gmail.com>
2024-07-19 10:20:08 +0530

    1096

---
+++ 1096A.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(int tc = 1)
+{
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int tc = 1;
+	cin >> tc;
+	for (int t = 0; t < tc; t++)
+	{
+		solve(t);
+	}
+}
